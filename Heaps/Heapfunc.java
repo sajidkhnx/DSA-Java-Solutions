9ba8113fa05758fc5bdf6e04d7e5a1b326178a23
Mohd Sajid Khan <khansahab9757@gmail.com>
2025-11-18 13:51:11 +0530

    Heaps

---
+++ Heaps/Heapfunc.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Heapfunc{
+   
+   static class Heap{ // insertion in heap 
+    ArrayList<Integer> arr = new ArrayList<>();
+     public void add (int data){
+        arr.add(data);
+        int x = arr.size()-1; // child  index
+        int par = (x-1)/2; // parent index
+        while(arr.get(x)<arr.get(par)){// child value < parent  since it is min heap  
+         // swap
+         int temp = arr.get(x);
+         arr.set(x,arr.get(par));
+         arr.set(par,temp);
+        } // 0(logn)
+     }
+     public int peak (){
+      return arr.get(0);
+     }
+
+     private void heapify(int i){
+      int left = 2*i+1;
+      int right = 2*i+2;
+      int minIdx = i;
+      if(left<arr.size()&&arr.get(minIdx)>arr.get(left)){
+         minIdx = right;
+      }
+      if(right<arr.size()&&arr.get(minIdx)>arr.get(right)){
+         minIdx = right;
+      }
+      if(minIdx!=i){
+         int temp = arr.get(i);
+         arr.set(i,arr.get(minIdx));
+         arr.set(minIdx,temp);
+
+         heapify(minIdx);
+      }
+     } 
+     public int remove(){
+      int data = arr.get(0);
+      // Step 1 : swap first and last
+      int temp = arr.get(0);
+      arr.set(0,arr.size()-1);
+      arr.set(arr.size()-1,temp);
+      // Step 2 : dalate last
+      arr.remove(arr.size()-1);
+      // Step 3 : Heapify
+      heapify(0);
+      return data;
+     }
+} 
+}
